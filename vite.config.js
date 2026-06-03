a0f2c089acb54729a97fd1b00e58c6dada0156aa
Tivva34 <tivva91@gmail.com>
2026-06-03 20:28:48 +0200

    Fixed path issue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MacService/',
+  base: '/MACService/',
   plugins: [
     react(),
   ],
